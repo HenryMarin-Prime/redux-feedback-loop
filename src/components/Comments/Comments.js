6835cdf7820ba74c44b3d76928d7d6c8c0b46dfe
HenryMarin-Prime <henrymarin@rocketmail.com>
2020-06-15 00:03:23 -0500

    review added, functions complete

---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import Review from '../Review/Review';
+
+class Comments extends Component {
+
+    state = {
+        comments: '',
+    }
+
+    handleChange = (event) => {
+        console.log( `in handleChange...`, event.target.value );
+        
+        this.setState({
+            ...this.state,
+            comments: event.target.value,
+        });
+    }
+
+    handleSubmit = () => {
+        console.log( `in handleSubmit...` );
+        const action = { type: 'ADD_COMMENTS', payload: this.state.comments };
+        this.props.dispatch( action );
+        this.props.history.push( '/review' );
+    }
+
+    render() {
+        return(
+            <div>
+                <h2>Please Leave Us A Comment</h2>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" 
+                            onChange={this.handleChange} />
+                    <button type="submit">Submit</button>
+                </form>
+
+                <br/>
+                <Review />
+            </div>
+        );
+    }
+}
+
+export default connect()(Comments);
