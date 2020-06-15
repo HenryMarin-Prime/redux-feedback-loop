eeced33ed18d74d42274242256f786139db7b1e6
HenryMarin-Prime <henrymarin@rocketmail.com>
2020-06-14 23:34:23 -0500

    get route added to routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,6 +2,18 @@ const express =require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-
+//server routes
+//get route
+router.get('/', (req,res)=>{
+    console.log('in router.get');
+    let queryText = `SELECT * FROM "feedback" ORDER BY "id" DESC;`;
+    pool.query(queryText).then((result)=>{
+        console.log('router.get SUCCESSFUL!!!', result.rows);
+        res.send(result.rows);
+    }).catch((error)=>{
+        console.log('Error in router.get', error);
+        res.sendStatus(500);
+    })
+})
 
 module.exports = router;
