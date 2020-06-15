ed54b5c3488e96e00f669a6137a8e444d91dc226
HenryMarin-Prime <henrymarin@rocketmail.com>
2020-06-14 23:29:36 -0500

    app.js file ready for work

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+
 
 class App extends Component {
+
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
+        <section>
+          
+        </section>
       </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default connect() (App);
